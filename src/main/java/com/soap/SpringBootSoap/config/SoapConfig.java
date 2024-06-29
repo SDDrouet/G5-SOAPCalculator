86fbd27f973139869d608e1ab0be33613813feb6
sddrouet <sddrouet@espe.edu.ec>
2024-06-29 11:46:13 -0500

    SOAP APP

---
+++ src/main/java/com/soap/SpringBootSoap/config/SoapConfig.java
@@ -0,0 +1,28 @@
+package com.soap.SpringBootSoap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+import com.soap.SpringBootSoap.client.SoapClient;
+
+@Configuration
+public class SoapConfig {
+
+    @Bean
+    public Jaxb2Marshaller marshaller(){
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("com.soap.SpringBootSoap.wsdl");
+        return marshaller;
+    }
+
+    @Bean
+    public SoapClient getSoapClient(Jaxb2Marshaller marshaller){
+        SoapClient soapClient = new SoapClient();
+        soapClient.setDefaultUri("http://www.dneonline.com/calculator.asmx");
+        soapClient.setMarshaller(marshaller);
+        soapClient.setUnmarshaller(marshaller);
+
+        return soapClient;
+    }
+}
